3f96c1ca3f504ab6519277b93d259daa2e59abf7
Kyle Cesare <kcesare@gmail.com>
2015-03-04 12:25:29 -0800

    Add new_from_air method to Ray.

--- src/ray.rs
+++ src/ray.rs
@@ -10,3 +10,13 @@ pub struct Ray {
     // The index of refraction of the material from which this ray is cast.
     pub index_of_refraction: f32
 }
+
+impl Ray {
+    fn new_from_air(origin: Pnt3<f32>, direction: Vec3<f32>) -> Ray {
+        Ray {
+            origin: origin,
+            direction: direction,
+            index_of_refraction: 1.000293
+        }
+    }
+}
