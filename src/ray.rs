1a06b1e8fcccc3687369b7a9bca8e7cf4e2b8a5c
Kyle Cesare <kcesare@gmail.com>
2015-03-04 12:26:10 -0800

    Comments.

--- src/ray.rs
+++ src/ray.rs
@@ -12,6 +12,8 @@ pub struct Ray {
 }
 
 impl Ray {
+    // Create a new ray from the given origin in the given direction where the
+    // index of refraction is assumed to be that of air.
     fn new_from_air(origin: Pnt3<f32>, direction: Vec3<f32>) -> Ray {
         Ray {
             origin: origin,
